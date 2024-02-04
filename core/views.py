05618a9e581793411ae1bc0e87d3ec3549d9a29f
mahadi025 <mahadikarimmunif@gmail.com>
2024-02-05 00:35:17 +0600

    updated the stylings

--- core/views.py
+++ core/views.py
@@ -103,12 +103,19 @@ def user_profile(request, pk):
 @login_required(login_url="login")
 def create_room(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == "POST":
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
         form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    context = {"form": form}
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get("name"),
+            description=request.POST.get("description"),
+        )
+        return redirect("home")
+    context = {"form": form, "topics": topics}
     return render(request, "core/room_form.html", context)
 
 
@@ -116,14 +123,18 @@ def create_room(request):
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse("You are not the host of this room!!")
     if request.method == "POST":
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    context = {"form": form}
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get("name")
+        room.topic = topic
+        room.description = request.POST.get("description")
+        room.save()
+        return redirect("home")
+    context = {"form": form, "topics": topics, "room": room}
     return render(request, "core/room_form.html", context)
 
 
