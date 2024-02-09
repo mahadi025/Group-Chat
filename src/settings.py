f9e87fbc70361bc8b2333a8a70abae014d1aa901
mahadi025 <mahadikarimmunif@gmail.com>
2024-02-09 22:39:23 +0600

    added assets

--- src/settings.py
+++ src/settings.py
@@ -116,14 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
 
-MEDIA_URL = "/images/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-# Corrected MEDIA_ROOT setting
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
