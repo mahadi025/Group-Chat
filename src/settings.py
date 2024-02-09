ff273b81a913070b57508dada6dcdadc7017e369
mahadi025 <mahadikarimmunif@gmail.com>
2024-02-09 22:27:21 +0600

    removed STATICFILES_DIRS

--- src/settings.py
+++ src/settings.py
@@ -120,11 +120,10 @@ STATIC_URL = "static/"
 
 MEDIA_URL = "/images/"
 
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_ROOT = BASE_DIR / "static/images"
+# Corrected MEDIA_ROOT setting
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
